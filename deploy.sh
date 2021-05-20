63135c0dc7edea61a5c85778284271ebf3f5501f
Dev-JamesR <admin@ether1.org>
2021-05-20 21:26:53 +0000

    Deploy Script Updates

--- deploy.sh
+++ deploy.sh
@@ -18,14 +18,14 @@ sudo systemctl stop ether1node && sudo systemctl disable ether1node
 sudo rm /etc/systemd/system/ether1node.service
 # Remove IPFS
 sudo rm /usr/sbin/ifps
-sudo rm -r $HOME/.ipfs
-sudo rm -r $HOME/.ether1
 sudo systemctl stop ipfs && sudo systemctl disable ipfs
 sudo rm /etc/systemd/system/ipfs.service
 # Remove ethoFS
 sudo rm /usr/sbin/ethoFS
 sudo systemctl stop ethoFS && sudo systemctl disable ethoFS
 sudo rm /etc/systemd/system/ethoFS.service
+sudo rm -r $HOME/.ipfs
+sudo rm -r $HOME/.ether1
 
 echo '**************************'
 echo 'Installing Etho Protcocol Node binary'
@@ -41,26 +41,27 @@ rm ether-1-linux-1.4.5.tar.gz
 sudo \mv geth /usr/sbin/
 
 echo '**************************'
-echo 'Initiating Etho Protocol Node
+echo 'Initiating Etho Protocol Node'
 echo '**************************'
-  cat > /tmp/ether1node.service << EOL
-  [Unit]
-  Description=Etho RPC Node
-  After=network.target
-  [Service]
-  User=$_user
-  Group=$_user
-  Type=simple
-  Restart=always
-  ExecStart=/usr/sbin/geth --syncmode=fast --cache=512 --datadir=$HOME/.ether1 -rpc --rpcapi "eth,net,web3" --rpcaddr 127.0.0.1 --rpcport 8545
-  [Install]
-  WantedBy=default.target
+
+cat > /tmp/ether1node.service << EOL
+[Unit]
+Description=Etho RPC Node
+After=network.target
+[Service]
+User=$_user
+Group=$_user
+Type=simple
+Restart=always
+ExecStart=/usr/sbin/geth --syncmode=fast --cache=512 --datadir=$HOME/.ether1 -rpc --rpcapi "eth,net,web3" --rpcaddr 127.0.0.1 --rpcport 8545
+[Install]
+WantedBy=default.target
 EOL
-  sudo \mv /tmp/ether1node.service /etc/systemd/system
-  sudo systemctl daemon-reload
-  sudo systemctl enable ether1node && systemctl start ether1node
-  sudo systemctl restart ether1node
-  sudo systemctl status ether1node --no-pager --full
+sudo \mv /tmp/ether1node.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable ether1node && systemctl start ether1node
+sudo systemctl restart ether1node
+sudo systemctl status ether1node --no-pager --full
 
 echo '**************************'
 echo 'Etho Protocol Node Setup Complete'
@@ -70,11 +71,12 @@ echo '**************************'
 wget https://dist.ipfs.io/go-ipfs/v0.8.0/go-ipfs_v0.8.0_linux-amd64.tar.gz
 tar -xzf go-ipfs_v0.8.0_linux-amd64.tar.gz
 chmod +x go-ipfs/ipfs
+rm go-ipfs_v0.8.0_linux-amd64.tar.gz
 sudo \mv go-ipfs/ipfs /usr/sbin/ipfs
 sudo rm -r go-ipfs
 
 echo '****************'
-echo 'Initialize IPFS
+echo 'Initialize IPFS'
 echo '****************'
 
 ipfs init
@@ -89,7 +91,7 @@ User=$_user
 Group=$_user
 Type=simple
 Restart=always
-ExecStart=/usr/sbin/ipfs console
+ExecStart=/usr/sbin/ipfs daemon
 [Install]
 WantedBy=default.target
 EOL
@@ -97,5 +99,4 @@ EOL
 sudo \mv /tmp/ipfs.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable ipfs && systemctl start ipfs
-sudo systemctl restart ipfs
 sudo systemctl status ipfs --no-pager --full
